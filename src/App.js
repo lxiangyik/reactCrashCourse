12d485598f73692ea66e71113bfd6c982f4c7afa
lxiangyik <assassinsboston@gmail.com>
2022-05-08 17:31:20 +0800

    online crash course

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import "./components/appStyles.css";
+// import styles from './components/appStyles.module.css'
+// import { Hello } from './components/Hello';
+// import {Message} from './components/Message';
+// import {ClickHandler} from './components/ClickHandler';
+// import { ParentComponent } from './components/ParentComponent';
+// import { UserGreeting } from './components/UserGreeting';
+// import { NameList } from './components/NameList';
+// import { StyleSheet } from './components/StyleSheet';
+// import { Inline } from './components/Inline';
+import { Form } from "./components/Form";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {/* <Hello name='jiahui' gender='male'> <p>Test</p></Hello>
+          <Hello name='Shanon' gender='female' />
+          <Message />
+          <ClickHandler /> */}
+          {/* <ParentComponent />
+          <UserGreeting />
+          <NameList />
+          <StyleSheet />
+          <Inline />
+          <h1 className='error'> Error</h1>
+          <h1 className={styles.success}> Success</h1> */}
+          <Form />
         </p>
-        <a
+        {/* <a
+
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noopener noreferrer" 
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
